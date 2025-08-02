4e4dfa43bc848bb7dcfd5b8b0038dbabc2878fa3
Imhhitt <diegovicentehernandez276@gmail.com>
2025-08-02 21:05:21 +0200

    chore: split koth loader and builder

--- src/main/java/dev/smartshub/shkoth/builder/KothBuilder.java
+++ src/main/java/dev/smartshub/shkoth/builder/KothBuilder.java
@@ -11,69 +11,59 @@ import dev.smartshub.shkoth.model.location.Corner;
 import dev.smartshub.shkoth.model.reward.PhysicalReward;
 import dev.smartshub.shkoth.model.time.Schedule;
 import dev.smartshub.shkoth.storage.file.Configuration;
-import dev.smartshub.shkoth.storage.file.FileManager;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
 
 public class KothBuilder {
 
     private final SchedulesBuilder schedulesBuilder = new SchedulesBuilder();
     private final PhysicalRewardsBuilder physicalRewardsBuilder = new PhysicalRewardsBuilder();
 
-    public Set<Koth> loadKothsFromConfig() {
+    public Koth buildKothFromFile(Configuration config) {
 
-        Set<Koth> koths = new HashSet<>();
+        String id = config.getName().replace(".yml", "");
+        String displayName = config.getString("display-name");
+        int maxDuration = config.getInt("max-duration");
+        int captureTime = config.getInt("capture-time");
 
-        for (Configuration config : FileManager.getAllFromFolder("koths")) {
+        Area area = new Area(
+                config.getString("world"),
+                new Corner(
+                        config.getInt("corner-1.x"),
+                        config.getInt("corner-1.y"),
+                        config.getInt("corner-1.z")
+                ),
+                new Corner(
+                        config.getInt("corner-2.x"),
+                        config.getInt("corner-2.y"),
+                        config.getInt("corner-2.z")
+                )
+        );
 
-            String id = config.getName().replace(".yml", "");
-            String displayName = config.getString("display-name");
-            int maxDuration = config.getInt("max-duration");
-            int captureTime = config.getInt("capture-time");
+        Mode mode = new Mode(
+                KothType.valueOf(config.getString("type")),
+                config.getInt("team-size")
+        );
 
-            Area area = new Area(
-                    config.getString("world"),
-                    new Corner(
-                            config.getInt("corner-1.x"),
-                            config.getInt("corner-1.y"),
-                            config.getInt("corner-1.z")
-                    ),
-                    new Corner(
-                            config.getInt("corner-2.x"),
-                            config.getInt("corner-2.y"),
-                            config.getInt("corner-2.z")
-                    )
-            );
-
-            Mode mode = new Mode(
-                    KothType.valueOf(config.getString("type")),
-                    config.getInt("team-size")
-            );
-
-            // Load schedules and rewards code is "dirty", doing it in a separate class to maintain clean code
-            List<Schedule> schedules = schedulesBuilder.getSchedulesFrom(config);
-            List<PhysicalReward> physicalRewards = physicalRewardsBuilder.getPhysicalRewardsFrom(config);
+        // Load schedules and rewards code is "dirty", doing it in a separate class to maintain clean code
+        List<Schedule> schedules = schedulesBuilder.getSchedulesFrom(config);
+        List<PhysicalReward> physicalRewards = physicalRewardsBuilder.getPhysicalRewardsFrom(config);
 
 
-            Commands commands = new Commands(
-                    config.getStringList("commands-perform.start"),
-                    config.getStringList("commands-perform.end"),
-                    config.getStringList("commands-perform.to-winners")
-            );
+        Commands commands = new Commands(
+                config.getStringList("commands-perform.start"),
+                config.getStringList("commands-perform.end"),
+                config.getStringList("commands-perform.to-winners")
+        );
 
-
-            Koth koth;
-            if (mode.type() == KothType.SOLO) {
-                koth = new SoloKoth(id, displayName, maxDuration, captureTime,area, mode, schedules, commands, physicalRewards);
-            } else {
-                koth = new TeamKoth(id, displayName, maxDuration, captureTime,area, mode, schedules, commands, physicalRewards);
-            }
-
-            koths.add(koth);
+        Koth koth;
+        if (mode.type() == KothType.SOLO) {
+            koth = new SoloKoth(id, displayName, maxDuration, captureTime,area, mode, schedules, commands, physicalRewards);
+        } else {
+            koth = new TeamKoth(id, displayName, maxDuration, captureTime,area, mode, schedules, commands, physicalRewards);
         }
 
-        return koths;
+        return koth;
     }
 }
---
+++ src/main/java/dev/smartshub/shkoth/loader/KothLoader.java
@@ -0,0 +1,25 @@
+package dev.smartshub.shkoth.loader;
+
+import dev.smartshub.shkoth.builder.KothBuilder;
+import dev.smartshub.shkoth.koth.Koth;
+import dev.smartshub.shkoth.storage.file.Configuration;
+import dev.smartshub.shkoth.storage.file.FileManager;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class KothLoader {
+
+    private final KothBuilder kothBuilder = new KothBuilder();
+
+    public Set<Koth> loadKoths(){
+        Set<Koth> koths = new HashSet<>();
+
+        for (Configuration config : FileManager.getAllFromFolder("koths")) {
+            koths.add(kothBuilder.buildKothFromFile(config));
+        }
+
+        return koths;
+    }
+
+}
