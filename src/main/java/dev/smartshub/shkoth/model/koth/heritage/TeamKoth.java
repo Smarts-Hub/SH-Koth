d4bea2597a7b33e3126ddfa9d2b8a5a90c6bf317
Imhhitt <diegovicentehernandez276@gmail.com>
2025-08-02 19:45:37 +0200

    feat: solo and team koth impl (wip)

---
+++ src/main/java/dev/smartshub/shkoth/model/koth/heritage/TeamKoth.java
@@ -0,0 +1,54 @@
+package dev.smartshub.shkoth.model.koth.heritage;
+
+import dev.smartshub.shkoth.model.koth.Koth;
+import dev.smartshub.shkoth.model.koth.command.Commands;
+import dev.smartshub.shkoth.model.koth.guideline.KothState;
+import dev.smartshub.shkoth.model.koth.guideline.Mode;
+import dev.smartshub.shkoth.model.koth.location.Area;
+import dev.smartshub.shkoth.model.koth.reward.PhysicalReward;
+import dev.smartshub.shkoth.model.koth.time.Schedule;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class TeamKoth extends Koth {
+
+    public TeamKoth(String id, String displayName, int duration, int captureTime, Area area, Mode mode, List<Schedule> schedules, Commands commands, List<PhysicalReward> physicalRewards) {
+        super(id, displayName, duration, captureTime, area, mode, schedules, commands, physicalRewards);
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void onPlayerEnter(Player player) {
+
+    }
+
+    @Override
+    public void onPlayerLeave(Player player) {
+
+    }
+
+    @Override
+    public boolean canPlayerCapture(Player player) {
+        return false;
+    }
+
+    @Override
+    protected void fireStateChangeEvent(KothState oldState, KothState newState) {
+
+    }
+}
