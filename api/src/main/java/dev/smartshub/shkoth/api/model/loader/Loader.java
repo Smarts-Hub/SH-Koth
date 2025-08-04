adbc7b619456224c27bf9c591e4e3e16f6088cd0
Imhhitt <diegovicentehernandez276@gmail.com>
2025-08-04 22:29:51 +0200

    refactor: loader to api

---
+++ api/src/main/java/dev/smartshub/shkoth/api/model/loader/Loader.java
@@ -0,0 +1,5 @@
+package dev.smartshub.shkoth.api.model.loader;
+
+public interface Loader<T>{
+    T load();
+}
--- src/main/java/dev/smartshub/shkoth/koth/loader/KothLoader.java
+++ src/main/java/dev/smartshub/shkoth/koth/loader/KothLoader.java
@@ -2,13 +2,14 @@ package dev.smartshub.shkoth.koth.loader;
 
 import dev.smartshub.shkoth.api.model.config.ConfigContainer;
 import dev.smartshub.shkoth.api.model.koth.Koth;
+import dev.smartshub.shkoth.api.model.loader.Loader;
 import dev.smartshub.shkoth.koth.builder.KothBuilder;
 import dev.smartshub.shkoth.storage.config.service.ConfigService;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class KothLoader {
+public class KothLoader implements Loader<Set<Koth>> {
 
     private final KothBuilder kothBuilder = new KothBuilder();
     private final ConfigService configService;
@@ -17,11 +18,12 @@ public class KothLoader {
         this.configService = configService;
     }
 
-    public Set<Koth> loadKoths(){
+    @Override
+    public Set<Koth> load(){
         Set<Koth> koths = new HashSet<>();
 
         for (ConfigContainer config : configService.provideAllKoths()) {
-            koths.add(kothBuilder.buildKothFromFile(config));
+            koths.add(kothBuilder.build(config));
         }
 
         return koths;
