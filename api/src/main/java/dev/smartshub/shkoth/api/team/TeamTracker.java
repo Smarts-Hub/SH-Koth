d10a77b3ad5e4d0fca6394a48ffa0ebecdd7d573
Imhhitt <diegovicentehernandez276@gmail.com>
2025-08-27 09:48:25 +0200

    fix: removed not tracking methods in order to make it flexible for implement hooks

--- api/src/main/java/dev/smartshub/shkoth/api/team/TeamTracker.java
+++ api/src/main/java/dev/smartshub/shkoth/api/team/TeamTracker.java
@@ -5,26 +5,18 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
-public interface TeamTracker {
+public interface TeamTracker<T> {
 
-    Team getTeamFrom(UUID uuid);
-    Team createTeam(UUID leader);
+    T getTeamFrom(UUID uuid);
+    T createTeam(UUID leader);
 
     Set<UUID> getTeamMembers(UUID anyTeamMember);
-    Collection<Team> getAllTeams();
-    Optional<Team> getTeamByLeader(UUID leader);
+    Collection<T> getAllTeams();
+    Optional<T> getTeamByLeader(UUID leader);
 
     boolean isTeamMember(UUID uuid);
     boolean isTeamLeader(UUID uuid);
     boolean areTeammates(UUID player1, UUID player2);
-    void dissolveTeam(UUID leader);
-
-    void addMember(UUID uuid, Team team);
-    void removeMember(UUID uuid);
-    void clearTeam(UUID uuid);
-    void clearAllTeams();
-
-    Team updateLeader(UUID oldLeader, UUID newLeader);
 
    String getTeamDisplayName(Team team);
 }
