ceb1b7740d4bc1275d9b6f823e197bdcc5f74f0f
Imhhitt <diegovicentehernandez276@gmail.com>
2025-09-29 21:52:31 +0200

    chore: api published to jitpack

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,9 +1,27 @@
 plugins {
     id("java-library")
+    id("maven-publish")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
     compileOnly("dev.triumphteam:triumph-gui:3.1.13")
     compileOnly("it.sauronsoftware.cron4j:cron4j:2.2.5")
+}
+
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = group.toString()
+            artifactId = "sh-koth-api"
+            version = project.version.toString()
+        }
+    }
 }
